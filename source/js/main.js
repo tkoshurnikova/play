efdbd7424ef6537c17e4ef080ad5d8ead83e2df8
Tanya <45432959+tkoshurnikova@users.noreply.github.com>
2020-11-10 14:40:04 +0300

    Добавляет текст манифестов

--- source/js/main.js
+++ source/js/main.js
@@ -1,20 +1,36 @@
-import {MANIFESTO_DONT, MANIFESTO_FOR} from './const.js';
+import {MANIFESTO_YA, MANIFESTO_FOR, MANIFESTO_DONT, MANIFESTO_EXCLAMATION, MANIFESTO_QUESTION} from './const.js';
 
 const form = document.querySelector(`form`);
 const result = document.querySelector(`.result`);
+const MANIFESTO_YA_MAX = MANIFESTO_YA.length;
 const MANIFESTO_DONT_MAX = MANIFESTO_DONT.length;
 const MANIFESTO_FOR_MAX = MANIFESTO_FOR.length;
+const MANIFESTO_EXCLAMATION_MAX = MANIFESTO_EXCLAMATION.length;
+const MANIFESTO_QUESTION_MAX = MANIFESTO_QUESTION.length;
+
 
 form.addEventListener(`submit`, (evt) => {
   evt.preventDefault();
 
   const number1 = Number(form.querySelector(`#number-1`).value);
   const result1 = result.querySelector(`#result-1`);
-  result1.textContent = MANIFESTO_DONT[number1 - 1];
+  result1.textContent = number1 ? MANIFESTO_YA[number1 - 1].text : ``;
 
   const number2 = Number(form.querySelector(`#number-2`).value);
   const result2 = result.querySelector(`#result-2`);
-  result2.textContent = MANIFESTO_FOR[number2 - 1];
+  result2.textContent = number2 ? MANIFESTO_DONT[number2 - 1].text : ``;
+
+  const number3 = Number(form.querySelector(`#number-3`).value);
+  const result3 = result.querySelector(`#result-3`);
+  result3.textContent = number3 ? MANIFESTO_FOR[number3 - 1].text : ``;
+
+  const number4 = Number(form.querySelector(`#number-4`).value);
+  const result4 = result.querySelector(`#result-4`);
+  result4.textContent = number4 ? MANIFESTO_EXCLAMATION[number4 - 1].text : ``;
+
+  const number5 = Number(form.querySelector(`#number-5`).value);
+  const result5 = result.querySelector(`#result-5`);
+  result5.textContent = number5 ? MANIFESTO_QUESTION[number5 - 1].text : ``;
 });
 
 form.addEventListener(`reset`, () => {
@@ -26,6 +42,9 @@ form.addEventListener(`reset`, () => {
 
 const randomValueButton = form.querySelector(`#random-value`);
 randomValueButton.addEventListener(`click`, () => {
-  form.querySelector(`#number-1`).value = Math.floor(Math.random() * MANIFESTO_DONT_MAX) + 1;
-  form.querySelector(`#number-2`).value = Math.floor(Math.random() * MANIFESTO_FOR_MAX) + 1;
+  form.querySelector(`#number-1`).value = Math.floor(Math.random() * MANIFESTO_YA_MAX) + 1;
+  form.querySelector(`#number-2`).value = Math.floor(Math.random() * MANIFESTO_DONT_MAX) + 1;
+  form.querySelector(`#number-3`).value = Math.floor(Math.random() * MANIFESTO_FOR_MAX) + 1;
+  form.querySelector(`#number-4`).value = Math.floor(Math.random() * MANIFESTO_EXCLAMATION_MAX) + 1;
+  form.querySelector(`#number-5`).value = Math.floor(Math.random() * MANIFESTO_QUESTION_MAX) + 1;
 })
